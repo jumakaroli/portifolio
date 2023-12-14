3f7a611fa02125209ea3c9eaa3da5dc25c1e0905
jumakaroli <juma@karolhosting.com>
2023-12-14 11:01:31 -0800

    this is CSS setup

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <h1 class="bg-brand-darker">Welcome to coding zone</h1>
+    <h1 class="bg-brand-darker text-white text-4xl">Welcome to coding zone</h1>
   </div>
 </template>
+<script setup>
+import '@unocss/reset/tailwind.css'
+
+</script>
